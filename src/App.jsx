b2a68d0bd0060609e8aa34b2f31cc0b488d0fecf
mwaidesu <machariamwai@gmail.com>
2023-08-21 18:27:27 +0300

    Finish set up of tailwind and google fonts

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import SideBar from './components/SideBar'
+
+function App() {
+
+  return (
+    <>
+    <main className="flex">
+    <SideBar/>
+
+    <div>
+      Components
+    </div>
+    </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+
+
+const SideBar = () => {
+  return (
+    <div>
+      Sidebar
+    </div>
+  )
+}
+
+export default SideBar
