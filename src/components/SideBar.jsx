e6bc32c6885e825d9acd98e12ef06992072d128a
mwaidesu <machariamwai@gmail.com>
2023-08-21 19:16:15 +0300

    Add Sidebar icons and styles

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,61 @@
-
+import {
+  ChartBarIcon,
+  HomeIcon,
+  DocumentSearchIcon,
+  MailIcon,
+  CreditCardIcon,
+  BellIcon,
+  ArrowUpIcon,
+  ExternalLinkIcon
+} from "@heroicons/react/solid";
 
 const SideBar = () => {
   return (
-    <div>
-      Sidebar
-    </div>
-  )
-}
+    <div className="bg-slate-800 flex-none w-14 sm:w-20 h-screen">
+      <div className="h-20 items-center flex">
+        <HomeIcon width={40} className="text-gray-300 left-3 sm:left-6 fixed" />
+      </div>
 
-export default SideBar
+      <div className="fixed left-3 sm:left-6 top-[100px]">
+        <ChartBarIcon
+          width={40}
+          className="bg-gray-600 p-2 rounded-lg  mb-4 text-gray-300"
+        />
+
+        <DocumentSearchIcon
+          width={40}
+          className="bg-gray-600 p-2 rounded-lg  mb-4 text-gray-300"
+        />
+
+        <MailIcon
+          width={40}
+          className="bg-gray-600 p-2 rounded-lg  mb-4 text-gray-300"
+        />
+
+        <CreditCardIcon
+          width={40}
+          className="bg-gray-600 p-2 rounded-lg  mb-4 text-gray-300"
+        />
+
+        <BellIcon
+          width={40}
+          className="bg-gray-600 p-2 rounded-lg  mb-4 text-gray-300"
+        />
+      </div>
+
+      <div className="fixed bottom-4 left-3 sm:left-6">
+        <ArrowUpIcon
+          width={40}
+          className="bg-gray-600 p-2 rounded-lg  mb-4 text-gray-300"
+        />
+
+        <ExternalLinkIcon
+          width={40}
+          className="bg-gray-600 p-2 rounded-lg  mb-4 text-gray-300"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
